434fa8fc2602018d8b1eef5019dea0f589632f84
zkoymen <zeynepkoymen9@gmail.com>
2025-07-11 14:41:29 +0300

    added new models --order and orderItem

---
+++ src/main/java/com/ecommerce/ecommerce_app/entities/Order.java
@@ -0,0 +1,49 @@
+package com.ecommerce.ecommerceapp.entities;
+
+
+import com.ecommerce.ecommerce_app.entities.User;
+import com.ecommerce.ecommerce_app.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    @PrePersist
+    protected void onCreate() {
+        orderDate = LocalDateTime.now();
+        status = OrderStatus.PENDING;
+    }
+}
